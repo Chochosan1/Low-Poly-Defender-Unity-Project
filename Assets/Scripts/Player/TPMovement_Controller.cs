56fccf42f52e7f7ec31dd255fcef6f3f94345377
Chochosan1 <angel.pashev@abv.bg>
2020-07-15 18:12:10 +0300

    Revert "Revert "Quest additions""
    This reverts commit f3e840fc92bcfc91d3b682e5411b9e0451fe2abe.

--- Assets/Scripts/Player/TPMovement_Controller.cs
+++ Assets/Scripts/Player/TPMovement_Controller.cs
@@ -30,6 +30,8 @@ public class TPMovement_Controller : MonoBehaviour
     public GameObject arrowToShootPrefab, arrowSpawnPoint, arrowFirePrefab;
     public UnityEngine.UI.Slider rageBar;
     public Animator rageBarAnim;
+    public GameObject rewardPanel;
+    private UnityEngine.UI.Text rewardPanelText;
     [Header("Cursor options")]
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
@@ -74,6 +76,7 @@ public class TPMovement_Controller : MonoBehaviour
     public float attack3Radius = 2f;
     private float internalAttack1Cooldown, internalAttack2Cooldown, internalAttack3Cooldown;
     private float internalAttack1Timestamp, internalAttack2Timestamp, internalAttack3Timestamp;
+    public bool bowUnlocked = false;
     public float maxSwitchEquipmentDuration;
     private float elapsedSwitchedEquipmentDuration;
 
@@ -125,6 +128,7 @@ public class TPMovement_Controller : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         playerCollider = GetComponent<CapsuleCollider>();
         anim = GetComponent<Animator>();
+        rewardPanelText = rewardPanel.GetComponentInChildren<UnityEngine.UI.Text>();
 
         Cursor.SetCursor(cursorTexture, cursorHotSpot, cursorMode);
         Cursor.lockState = CursorLockMode.Locked;
@@ -544,6 +548,21 @@ public class TPMovement_Controller : MonoBehaviour
         is_Grounded = true;
     }
 
+    public void UnlockBow()
+    {
+        bowUnlocked = true;
+        rewardPanel.SetActive(true);
+        rewardPanelText.text = "Bow unlocked!";
+        StartCoroutine(DisableGameObjectAfter(rewardPanel, 3f));
+        Debug.Log("Reward acquired: bow");
+    }
+
+    private IEnumerator DisableGameObjectAfter(GameObject objectToDisable, float disableAfterSeconds)
+    {
+        yield return new WaitForSeconds(disableAfterSeconds);
+        objectToDisable.SetActive(false);
+    }
+
     public void Attack1()
     {
         // Does the ray intersect any objects in the specified layer
@@ -886,9 +905,12 @@ public class TPMovement_Controller : MonoBehaviour
 
     private void HandleEquipmentSwitch(InputAction.CallbackContext context)
     {
-        elapsedSwitchedEquipmentDuration = 0f;
-        movementState = MovementState.SwitchingEquipment;      
-        anim.SetBool("is_EquipmentSwitch", true);
+        if(bowUnlocked)
+        {
+            elapsedSwitchedEquipmentDuration = 0f;
+            movementState = MovementState.SwitchingEquipment;
+            anim.SetBool("is_EquipmentSwitch", true);
+        }    
     }
     #endregion
 }
