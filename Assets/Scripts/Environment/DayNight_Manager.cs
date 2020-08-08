b218c445a67af9a5ec569777ed74a8d3ebe56af9
Chochosan1 <angel.pashev@abv.bg>
2020-08-08 15:26:32 +0300

    Day/night cycle massive improvements
    Full control over the shaders' exposures;
    Right click on the script and navigate to Chochosan->Reset exposure to reset the exposures in the editor;
    Switching states transitions nicely, not entirely seamless but still cool

--- Assets/Scripts/Environment/DayNight_Manager.cs
+++ Assets/Scripts/Environment/DayNight_Manager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public enum CurrentTimeState { Day, Night}
 /// <summary>
-/// TODO: LERP SHADER SKYBOX EXPOSURE AS WELL
+/// Lerped day/night cycle based on a duration. Context menu item created for an easy reset to the original form of the shaders.
 /// </summary>
 public class DayNight_Manager : MonoBehaviour
 {
     public static DayNight_Manager Instance;
 
     [SerializeField]
+    [Tooltip("All lights that must be enabled/disabled when swapping from day to night and vice versa.")]
     private List<Light> lightsList;
 
     private void Awake()
@@ -24,15 +25,23 @@ public class DayNight_Manager : MonoBehaviour
     [SerializeField]
     private Material nightSkybox, daySkybox;
     [SerializeField]
+    [Tooltip("The main light.")]
     private Light sunLight;
     private Color32 daySunColor;
     private Color32 nightSunColor;
     private Color32 ambientDayColor;
     private Color32 ambientNightColor;
     [SerializeField]
+    [Tooltip("How long it will take to swap between day and night.")]
     private float cycleDuration = 5f;
+    [SerializeField]
+    [Range(0, 1)]
+    [Tooltip("After switching to another time state (day/night), the cycle will pause for a certain duration (in order to keep the game longer in the darkest/brightest state). It is a percentage based on the cycle duration time.")]
+    private float cyclePauseMultiplier = 0.2f;
+    private float cyclePauseDuration; //the final result after applying the pauseMultiplier to the duration
+    private bool isCurrentlyPaused;
+    private bool isLightsOn;
     private float elapsedCycleTime = 0f;
-    float step = 0f;
 
     private CurrentTimeState currentTimeState;
 
@@ -44,73 +53,97 @@ public class DayNight_Manager : MonoBehaviour
         nightSunColor = new Color32(0, 0, 0, 255);
 
         ambientDayColor = new Color32(152, 152, 152, 255);
-        ambientNightColor = new Color32(90, 90, 90, 255);
+        ambientNightColor = new Color32(30, 30, 30, 255);
+
+        cyclePauseDuration = cycleDuration * cyclePauseMultiplier;
+        Debug.Log("Pause is " + cyclePauseDuration);
+        isCurrentlyPaused = false;
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.N))
-        {
-            ToggleTimeState();
-        }
+        //if(Input.GetKeyDown(KeyCode.N))
+        //{
+        //    ToggleTimeState();
+        //}
+        if (isCurrentlyPaused)
+            return;
 
         elapsedCycleTime += Time.deltaTime;
         
-
         if (currentTimeState == CurrentTimeState.Day && elapsedCycleTime <= cycleDuration)
         {
+            //lerp from Day brightness to Night darkness
             RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, ambientNightColor, Time.deltaTime / cycleDuration);
-            sunLight.color = Color.Lerp(sunLight.color, nightSunColor, Time.deltaTime / cycleDuration);
-            daySkybox.SetFloat("_Exposure", Mathf.Lerp(daySkybox.GetFloat("_Exposure"), 0.2f, Time.deltaTime / cycleDuration));
-        }
-        else if(currentTimeState == CurrentTimeState.Night && elapsedCycleTime <= cycleDuration)
-        {
-            RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, ambientDayColor, Time.deltaTime / cycleDuration);
-            sunLight.color = Color.Lerp(sunLight.color, daySunColor, Time.deltaTime / cycleDuration);
-            nightSkybox.SetFloat("_Exposure", Mathf.Lerp(daySkybox.GetFloat("_Exposure"), 1.2f, Time.deltaTime / cycleDuration));
-        }
-        else
-        {      
-            if(currentTimeState == CurrentTimeState.Day)
+            sunLight.color = Color.Lerp(sunLight.color, nightSunColor, Time.deltaTime / cycleDuration * 2);
+            RenderSettings.reflectionIntensity = Mathf.Lerp(RenderSettings.reflectionIntensity, 0, Time.deltaTime / cycleDuration * 2);
+            daySkybox.SetFloat("_Exposure", Mathf.Lerp(daySkybox.GetFloat("_Exposure"), 0.2f, Time.deltaTime / cycleDuration * 2));
+
+            //after a certain stage is reached and if the lights are off then turn them on although it is still during the day phase 
+            if(elapsedCycleTime / cycleDuration >= 0.8 && !isLightsOn)
             {
-                currentTimeState = CurrentTimeState.Night;
-                RenderSettings.reflectionIntensity = 0;
-                sunLight.shadows = LightShadows.Hard;
-                RenderSettings.skybox = nightSkybox;
-                nightSkybox.SetFloat("_Exposure", 0.78f);
                 foreach (Light light in lightsList)
                 {
                     light.gameObject.SetActive(true);
                 }
-                Debug.Log("ITS NIGHT");
+                isLightsOn = true;
             }
-            else
+        }
+        else if(currentTimeState == CurrentTimeState.Night && elapsedCycleTime <= cycleDuration)
+        {
+            //lerp from Night darkness to Day brightness
+            RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, ambientDayColor, Time.deltaTime / cycleDuration);
+            sunLight.color = Color.Lerp(sunLight.color, daySunColor, Time.deltaTime / cycleDuration * 2);
+            RenderSettings.reflectionIntensity = Mathf.Lerp(RenderSettings.reflectionIntensity, 1, Time.deltaTime / cycleDuration * 2);
+            nightSkybox.SetFloat("_Exposure", Mathf.Lerp(nightSkybox.GetFloat("_Exposure"), 2f, Time.deltaTime / cycleDuration * 4));
+
+            //after a certain stage is reached and if the lights are on then turn them off and put on the day skybox 
+            //with a lower than normal exposure (to make a seamless transition) although it is still during the night phase
+            if (elapsedCycleTime / cycleDuration >= 0.65 && isLightsOn)
             {
-                currentTimeState = CurrentTimeState.Day;
-                RenderSettings.reflectionIntensity = 1;
-                sunLight.shadows = LightShadows.Soft;
-                RenderSettings.skybox = daySkybox;
-                daySkybox.SetFloat("_Exposure", 1.2f);
                 foreach (Light light in lightsList)
                 {
                     light.gameObject.SetActive(false);
                 }
-                Debug.Log("ITS DAY");
+                isLightsOn = false;
+                RenderSettings.skybox = daySkybox;
+                daySkybox.SetFloat("_Exposure", 0.75f);
             }
-            step = 0f;
-            elapsedCycleTime = 0f;
-            Debug.Log("CYCLE RESET");
+
+            //if lights are off and the day skybox is on during the night phase then increase the exposure before officially 
+            //switching to day state
+            if(!isLightsOn)
+            {
+                daySkybox.SetFloat("_Exposure", Mathf.Lerp(daySkybox.GetFloat("_Exposure"), 1.2f, Time.deltaTime / cycleDuration * 3));
+            }
         }
-        
-            
-        //if (elapsedCycleTime >= cycleDuration)
-        //{
-        //    elapsedCycleTime = 0f;
-        //    // ToggleTimeState();
-        //}
+        else
+        {      
+            //switch the time state and set the settings accordingly in case something has not lerped fully to the desired values
+            //(lerping will still make the transitions smooth)
+            if(currentTimeState == CurrentTimeState.Day)
+            {
+                SetNightSettings();
+            }
+            else
+            {
+                SetDaySettings();
+            }
+            elapsedCycleTime = 0f;
+            StartCoroutine(PauseAndResumeCycleAfter(cyclePauseDuration));
+        }
     }
 
-    public void ToggleTimeState()
+    //right click on the script to quickly reset the exposures through the editor
+    [ContextMenu("Chochosan/Reset skybox exposures")]
+    public void ResetSkyboxExposures()
+    {
+        nightSkybox.SetFloat("_Exposure", 0.7f);
+        daySkybox.SetFloat("_Exposure", 1.2f);
+        Debug.Log("ChochosanTools: Skybox exposures reset.");
+    }
+
+    private void ToggleTimeState()
     {
         switch(currentTimeState)
         {
@@ -125,36 +158,41 @@ public class DayNight_Manager : MonoBehaviour
 
     private void SetDaySettings()
     {
-        RenderSettings.ambientLight = ambientDayColor;
+        SetCurrentTimeState(CurrentTimeState.Day);
         RenderSettings.reflectionIntensity = 1;
-        sunLight.color = daySunColor;
         sunLight.shadows = LightShadows.Soft;
         RenderSettings.skybox = daySkybox;
-        currentTimeState = CurrentTimeState.Day;
-
-        foreach(Light light in lightsList)
+        daySkybox.SetFloat("_Exposure", 1.2f);
+        foreach (Light light in lightsList)
         {
             light.gameObject.SetActive(false);
         }
+        Debug.Log("ITS DAY");
     }
 
     private void SetNightSettings()
-    {      
-        RenderSettings.ambientLight = ambientNightColor;
+    {
+        SetCurrentTimeState(CurrentTimeState.Night);
         RenderSettings.reflectionIntensity = 0;
-        sunLight.color = nightSunColor;
         sunLight.shadows = LightShadows.None;
         RenderSettings.skybox = nightSkybox;
-        currentTimeState = CurrentTimeState.Night;
-
+        nightSkybox.SetFloat("_Exposure", 0.7f);
         foreach (Light light in lightsList)
         {
             light.gameObject.SetActive(true);
         }
+        Debug.Log("ITS NIGHT");
     }
 
-    private void SetCurrentTimeState(CurrentTimeState timeState)
+    private IEnumerator PauseAndResumeCycleAfter(float pauseDuration)
     {
-        currentTimeState = timeState;
+        isCurrentlyPaused = true;
+        Debug.Log("Cycle paused.");
+        yield return new WaitForSeconds(pauseDuration);
+        Debug.Log("Cycle resumed");
+        isCurrentlyPaused = false;
     }
+
+    private void SetCurrentTimeState(CurrentTimeState timeState) => currentTimeState = timeState;
+    public CurrentTimeState GetCurrentTimeState() => currentTimeState;
 }
